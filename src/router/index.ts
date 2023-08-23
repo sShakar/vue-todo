d7825a8e4c0fcdb92bafa975d1ef6bee8316bbc1
Shakar Kosar <70109216+sShakar@users.noreply.github.com>
2023-08-23 10:37:05 +0300

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HomeView from '@/views/HomeView.vue';
-import DeletedView from '@/views/DeletedView.vue';
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // It's better to create a separate file for the routes.
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('@/views/HomeView.vue')
     },
     {
       path: '/deleted',
       name: 'deleted',
-      component: DeletedView
+      component: () => import('@/views/DeletedView.vue')
     }
   ]
 });
